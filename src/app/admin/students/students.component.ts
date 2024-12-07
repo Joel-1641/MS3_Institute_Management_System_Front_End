839455a739d75fc0aea0926ce106b83f20e4e5c9
Joel-1641 <ut01641tic2024@gmail.com>
2024-12-07 21:41:28 +0530

    Admin Dashboard student component created

---
+++ src/app/admin/students/students.component.ts
@@ -0,0 +1,52 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StudentService, Student } from '../../services/student.service';
+
+
+@Component({
+  selector: 'app-students',
+  standalone: true,
+  templateUrl: './students.component.html',
+  styleUrls: ['./students.component.css'],
+  imports: [CommonModule],
+})
+export class StudentsComponent implements OnInit{
+  private studentService = inject(StudentService);
+  students: Student[] = [];
+  isLoading = true;
+  errorMessage = '';
+  constructor() {}
+
+  ngOnInit(): void {
+    this.fetchStudents();
+  }
+
+  fetchStudents(){
+    this.studentService.getStudents().subscribe({
+      next: (data) => {
+        this.students = data;
+        console.log(this.students)
+       this.isLoading = false; 
+      },
+      error: (error) => {
+        this.errorMessage = 'Failed to load students. Please try again later.';
+        this.isLoading = false;
+      }
+    });
+  }
+  
+  // students = [
+  //   {
+  //     studentId: 1,
+  //     userId: 0,
+  //     fullName: "Joel",
+  //     email: "jo@gmail.com",
+  //     profilePicture: null,
+  //     nicNumber: "983091113V",
+  //     gender: "Male",
+  //     address: "string",
+  //     mobileNumber: 772389081,
+  //     dateOfBirth: "2004-12-07T05:46:03.545",
+  //   }
+  // ];
+}
